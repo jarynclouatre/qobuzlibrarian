15b09b136eab29fe4b9d7a2e8d9678662d0561c4
jarynclouatre <jarynclouatre@proton.me>
2026-03-07 17:53:28 -0600

    bump version to 0.4.0 and date the changelog

--- src/qobuz_librarian/web/app.py
+++ src/qobuz_librarian/web/app.py
@@ -259,7 +259,7 @@ try:
     from importlib.metadata import version as _pkg_version
     _APP_VERSION = _pkg_version("qobuz-librarian")
 except Exception:
-    _APP_VERSION = "0.3.0"
+    _APP_VERSION = "0.4.0"
 templates.env.globals["app_version"] = _APP_VERSION
 templates.env.globals["repo_url"] = "https://github.com/jarynclouatre/qobuz-librarian"
 
